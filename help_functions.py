179ce37546b6f770e22e04183ab2ab52d04986f1
metterytt <metterytt@live.dk>
2019-06-19 09:23:45 +0200

    Rettelser regex m.m.

--- help_functions.py
+++ help_functions.py
@@ -6,6 +6,7 @@ from multiprocessing import Pool, cpu_count
 # url = sys.argv[1:]
 
 def url_to_transcript_parallel(urls):
+    '''Returns transcripts from millercenter.org, running the function url_to_transcript in parallel.'''
     workers = cpu_count()
     pool = Pool(processes=workers)
     result = pool.map(url_to_transcript, urls)
@@ -16,10 +17,13 @@ def url_to_transcript(url):
     page = requests.get(url).text
     soup = BeautifulSoup(page, "lxml")
     text = [p.text for p in soup.find(class_="transcript-inner").find_all('p')]
-#     print('Downloading transcript from ' + url)
     return text
 
 def combine_text(list_of_text):
     '''Takes a list of strings and combines them into one single string.'''
     combined_text = ' '.join(list_of_text)
     return combined_text
+
+
+# It took 7.89242148399353s in total. No Multiprocessing
+# It took 2.926537275314331s in total. Multiprocessing
